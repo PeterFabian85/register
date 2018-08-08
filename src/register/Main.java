2b952d257b13227c8f75bf12aaf4d2c412efafaa
student <tomassiman7@gmail.com>
2018-08-08 13:50:14 +0200

    first commit

---
+++ src/register/Main.java
@@ -0,0 +1,26 @@
+package register;
+
+import register.persist.RegisterDbStorage;
+import register.persist.RegisterFileStorage;
+import register.persist.RegisterStorage;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        //Register register = new ArrayRegister(3);
+    	RegisterStorage storage = new RegisterDbStorage();
+
+    	Register register = storage.load();
+
+    	if (register.getCount() == 0) {
+			register.addPerson(new Person("Janko Hrasko", "0900123456"));
+	    	register.addPerson(new Person("Jurko", "546548"));
+	    	register.addPerson(new Person("Luki", "545844"));
+    	}
+    	
+        ConsoleUI ui = new ConsoleUI(register);
+        ui.run();
+        
+        storage.save(register);
+    }
+}
